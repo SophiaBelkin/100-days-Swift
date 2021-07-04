26c9c8b2b28d15b87beb31425e62a442360009ed
S <sophia.lu@Sophias-Mini.socal.rr.com>
2021-07-04 16:55:48 -0700

    Added new projects

---
+++ SwiftUI/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sophia Lu on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
